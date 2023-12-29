64a7bd632752a96570eef039bdde9d17b2eb6c03
Lacielton Brito <lacielton.brito@voipir.cl>
2023-12-29 17:34:35 -0300

    starting the frontend

--- src/tasks-web/src/index.ts
+++ src/tasks-web/src/index.ts
@@ -5,4 +5,18 @@
 
 let main_div = <HTMLDivElement>document.getElementById("main");
 
-main_div.textContent = "Hello World!";
+let label  = document.createElement("label");
+let button = document.createElement("button");
+
+button.textContent = "Press me!";
+
+button.onclick = (): void => {
+    if (label.textContent == "") {
+        label.textContent = "I was just clicked!";
+    } else {
+        label.textContent = "";
+    }
+};
+
+main_div.appendChild(label);
+main_div.appendChild(button);
